13550f9f898f6fe800b81169be74ed6d36c7854b
Paul Young <yyang173@illinois.edu>
2016-12-13 13:29:16 -0600

    moved epl_val_err to qpr

--- forces.py
+++ forces.py
@@ -1,25 +1,4 @@
 #!/usr/bin/env python
-
-import pandas as pd
-def epl_val_err(epl_out):
-    """ epl_out is expected to be an output of energy.pl from QMCPACK
-     It simply has to have the format {name:22c}={val:17.3f} +/- {err:26.4f}.
-     rows with forces will be recognized with 'force_prefix' """
-    tab = pd.read_table(epl_out,delimiter='=',names=['name','text'])
-    tab = tab.dropna()
-    
-    def text2val(text):
-        tokens = text.split('+/-')
-        if len(tokens) != 2:
-            raise NotImplementedError('unrecognized value '+text)
-        # end if
-        val,err = map(float,tokens)
-        return pd.Series({'val':val,'err':err})
-    # end def text2val
-    
-    df = pd.concat( [tab.drop('text',axis=1),tab['text'].apply(text2val)],axis=1 )
-    return df
-
 def plot_forces(ax,epl_df,force_prefix='force_'):
     forces = epl_df[ epl_df['name'].apply(lambda x:x.startswith(force_prefix)) ]
         
@@ -39,7 +18,8 @@ if __name__ == '__main__':
     epl_out = args.epl_out
     fpre    = args.force_prefix
 
-    epl_df  = epl_val_err(epl_out)
+    import qmcpack_reader as qpr
+    epl_df  = qpr.epl_val_err(epl_out)
     
     import matplotlib.pyplot as plt
     fig,ax = plt.subplots(1,1)
