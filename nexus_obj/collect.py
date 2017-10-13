e644abdec91b1e88071c2019432298baa4b67983
Paul Young <yyang173@illinois.edu>
2017-10-13 14:38:05 -0500

    map dmc to scf

--- nexus_obj/collect.py
+++ nexus_obj/collect.py
@@ -17,6 +17,7 @@ def collect_qmc_sims(qmc_sims):
     qa = qmc.load_analyzer_image()
     for iqmc in qa['qmc'].keys():
       entry = qa['qmc'][iqmc]['scalars'].to_dict()
+      entry['path'] = qmc.path
       qmc_data.append( entry )
     # end for
   # end for
@@ -24,43 +25,52 @@ def collect_qmc_sims(qmc_sims):
   return qmc_df
 # end def
 
-def orb_nscf_input(sdmc):
+def orb_scf_input(sdmc):
   """ find the scf inputs used to generate sdmc """
+  myinputs = None # this is the goal
+  sdep = 'dependencies' # string representation of the dependencies entry
 
   # step 1: find the p2q simulation id
   p2q_id = None
-  for key in sdmc['dependencies'].keys():
-    if sdmc['dependencies'][key].result_names[0] == 'orbitals':
+  for key in sdmc[sdep].keys():
+    if sdmc[sdep][key].result_names[0] == 'orbitals':
       p2q_id = key
     # end if
   # end for dep
 
-  # step 2: find the nscf simulation id
-  nscf_id_list = sdmc['dependencies'][p2q_id]['sim']['dependencies'].keys()
+  # step 2: find the nscf simulation
+  nscf_id_list = sdmc[sdep][p2q_id]['sim'][sdep].keys()
   assert len(nscf_id_list) == 1
   nscf_id = nscf_id_list[0]
+  nscf = sdmc[sdep][p2q_id]['sim'][sdep][nscf_id]
+  myinputs = nscf['sim']['input']
 
-  # this may actually be the scf simulation
-  nscf = sdmc['dependencies'][p2q_id]['sim']['dependencies'][nscf_id]
-  nscf_inputs = nscf['sim']['input']
-  return nscf_inputs
-  # nscf_inputs should be good enough
-  ###if (nscf_inputs['control']['calculation'] == 'scf'):
-  ###  return nscf_inputs
-  #### end if
-
-  ###raise NotImplementedError('sdmc p2q_id=%d nscf_id=%d, need to find scf_id'%(p2q_id,nscf_id))
-  #### if nscf is not the scf, then we need to go deeper
-
-# end def
+  # step 3: find the scf simulation
+  calc = myinputs['control']['calculation']
+  if (calc=='scf'): # scf may actually be the scf simulation
+    pass # myinputs is already set
+  elif (calc=='nscf'): # if nscf is not the scf, then we need to go deeper
+    scf_id = nscf['sim'][sdep].keys()[0] 
+    scf = nscf['sim'][sdep][scf_id]
+    myinputs = scf['sim']['input'] # this is it!
+    scalc = myinputs['control']['calculation']
+    if scalc != 'scf':
+      RuntimeError('nscf depends on %s instead of scf'%scalc)
+    # end if
+  else:
+    raise RuntimeError('unknown simulation type %s'%calc)
+  # end if
+  
+  return myinputs.to_dict()
+# end def orb_scf_input
 
 def qmc_scf_path_map(qmc_sims):
   data = []
   for qmc in qmc_sims:
-    scf_inputs = orb_nscf_input(qmc)
-    entry = {'qmc_path':qmc.path,'scf_inputs':scf_inputs}
+    myinputs = orb_scf_input(qmc)
+    entry = {'qmc_path':qmc.path,'nscf_inputs':dict(myinputs)}
+    data.append(entry)
   # end for qmc
-  df = pd.DataFrame(data)
+  df = pd.DataFrame(data).set_index('qmc_path')
   return df
 # end def qmc_scf_path_map
-
