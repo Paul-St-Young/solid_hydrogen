0598000f3f21f04872720c87282cb3b41906b3e4
Paul Young <yyang173@illinois.edu>
2018-05-16 18:56:52 -0500

    get_axes_pos and get_density

--- qharv_db/meta.py
+++ qharv_db/meta.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
 import pandas as pd
 from qharv.reel import mole
+
+# ======================== level 0: paths and inputs =========================
 
 
 def get_pcdirs(
@@ -8,7 +11,22 @@ def get_pcdirs(
     pd_name='proj_dir.dat',
     fdl_name='folder_list.dat'
   ):
-  """ read project dir and calc dirs from inputs """
+  """ read project dir and calc dirs from inputs
+  default to the following directory structure:
+  $ls -R
+  ./inp_text:
+  folder_list.dat  proj_dir.dat
+
+  proj_dir.dat should hold the absolute path to project folder
+  folder_list.dat should hold a list of QMC calculation directories
+
+  Args:
+    tmp_dir (str, optional): temporary folder to hold inputs and meta data
+    pd_name (str, optional): file holding project directory
+    fdl_name (str, optional): file holding calculation folder list
+  Return:
+    tuple: (str, list) i.e. (proj_dir, folder_list)
+  """
 
   # locate project directory
   pd_dat  = os.path.join(tmp_dir, pd_name)
@@ -23,7 +41,19 @@ def get_pcdirs(
 
 
 def collect_first_input(folderl):
-  """ collect input text into a database """
+  """ collect input text into a database
+
+  example:
+    proj_dir, fdl = get_pcdirs()
+    folderl = [os.path.join(proj_dir, folder) for folder in fdl]
+    mdf = collect_first_input(folderl)
+
+  Args:
+    folderl (list): a list of QMC calculation folders
+  Return:
+    pd.DataFrame: mdf contains ['path', 'inp_text'] columns. mdf collects the
+    first input xml mole finds in each folder.
+  """
   data = []
   for folder in folderl:
     fin = mole.files_with_regex('*.in.xml',folder)[0]
@@ -51,3 +81,24 @@ def find_all_groups_and_series(folderl):
   # end for
   gsdf = pd.DataFrame(gsdata).sort_values('group')
   return gsdf
+
+
+# ====================== level 1: parse input =======================
+
+
+def get_axes_pos(doc):
+  from qharv.seed import xml
+  axes = xml.get_axes(doc)
+  pos = xml.get_pos(doc)
+  entry = {'axes':axes.tolist(), 'pos':pos.tolist()}
+  return entry
+
+
+def get_density(doc):
+  from qharv.inspect import axes_pos
+  entry = get_axes_pos(doc)
+  volume = axes_pos.volume(entry['axes'])
+  natom = len(entry['pos'])
+  rho = natom/volume
+  rs = (3./(4*np.pi*rho))**(1./3)
+  return {'rs':rs, 'volume':volume, 'rho':rho}
