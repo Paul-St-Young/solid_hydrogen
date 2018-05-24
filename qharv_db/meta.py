1cb7bbfc8d0d71f5d4820dd2ff5fbc779a2c1b81
Paul Young <yyang173@illinois.edu>
2018-05-24 16:22:22 -0500

    get_prefix_from_path

--- qharv_db/meta.py
+++ qharv_db/meta.py
@@ -83,6 +83,13 @@ def find_all_groups_and_series(folderl):
   return gsdf
 
 
+def get_prefix_from_path(path, proj_dir, sep='_'):
+  task_dir = path.replace(proj_dir, '')
+  prefix = sep.join([seg for seg in task_dir.split('/')
+    if seg not in ['.', '..']]).strip(sep)
+  return prefix
+
+
 # ====================== level 1: parse input =======================
 
 
