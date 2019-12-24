7e112e17f8e25574f6b526566a4990c6580d5610
Paul Young <yyang173@illinois.edu>
2019-12-24 15:14:39 -0600

    ysum_ysq_count

--- qharv_db/grsk.py
+++ qharv_db/grsk.py
@@ -70,3 +70,41 @@ def gofr_norm(myr, natom, volume):
   # assemble norm vector
   nvec = 1./(rho*vnorm)
   return nvec
+
+def ysum_ysq_count(ysum, ysq, counts):
+  """ calculate mean and error given accumulated sum of val and sq
+
+  Args:
+    ysum (np.array): accumulated sum of values, (nentry,)
+    ysq (np.array): accumulated sum of squares, (nentry,)
+    counts (np.array): number of hits for each entry, (nentry,)
+  Return:
+    (np.array, np.array, np.array): (sel, ym, ye),
+      (valid entries, mean, error)
+  Example:
+    >>> nr = len(myr)
+    >>> ysum = np.zeros(nr)
+    >>> ysq = np.zeros(nr)
+    >>> csum = np.zeros(nr, dtype=int)
+    >>> csq = np.zeros(nr, dtype=int)
+    >>> for box, pos, vec in zip(boxl, posl, vecl):
+    >>>   drij = displacement_table(pos, box)
+    >>>   y1, c1 = gofv(myr, drij, vec)
+    >>>   ysum += y1
+    >>>   ysq += y1**2
+    >>>   csum += c1
+    >>>   csq += c1**2
+    >>> sel, ym, ye = ysum_ysq_count(ysum, ysq, csum)
+    >>> myx = myr[sel]
+    >>> # g(r)
+    >>> grcnt = len(boxl)*np.ones(len(myy))
+    >>> sel1, grm, gre = ysum_ysq_count(csum, csq, grcnt)
+    >>> nvec = gofr_norm(myr, len(drij), volume)
+    >>> grm *= nvec; gre *= nvec
+  """
+  sel = counts > 1  # need to divide by counts-1
+  ym = ysum[sel]/counts[sel]
+  y2 = ysq[sel]/counts[sel]
+  ye2 = (y2-ym**2)/(counts[sel]-1)
+  ye = ye2**0.5
+  return sel, ym, ye
