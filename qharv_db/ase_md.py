bf5b19eabb631984ee497e7d107bec951b80ffb5
Paul Young <yyang173@illinois.edu>
2020-05-26 13:37:16 -0500

    fix hcp primitive cell

--- qharv_db/ase_md.py
+++ qharv_db/ase_md.py
@@ -16,7 +16,7 @@ def hcp_prim_cell(a, ca):
   pos = np.dot(
     np.array([
       [0, 0, 0],
-      [2./3, 1./3, 1./2]
+      [1./3, 1./3, 1./2]
     ]), axes)
 
   atoms = Atoms('H%d' % len(pos), cell=axes, positions=pos, pbc=1)
